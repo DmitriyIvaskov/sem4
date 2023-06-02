2accfd51262f6615c418da73024152fa67ebe9b0
DmitriyIvaskov <citrys2008@gmail.com>
2023-06-02 20:16:48 +0700

    Ex26

---
+++ Ex 26/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+//Console.WriteLine("Ведите Число");
+//int sumNumbers = Convert.ToInt32(Console.ReadLine())
+
+Console.WriteLine("Введите число ");
+string input = Console.ReadLine();
+Console.WriteLine($"Количество цифр в строке {input.Length}"); 
