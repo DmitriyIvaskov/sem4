cd7cab96be23a241eb993a1c73f66c3f01552638
DmitriyIvaskov <citrys2008@gmail.com>
2023-06-02 20:50:16 +0700

    Ex26 оно заработало

--- Ex 26/Program.cs
+++ Ex 26/Program.cs
@@ -6,6 +6,24 @@
 //Console.WriteLine("Ведите Число");
 //int sumNumbers = Convert.ToInt32(Console.ReadLine())
 
-Console.WriteLine("Введите число ");
+/*Console.WriteLine("Введите число ");
 string input = Console.ReadLine();
-Console.WriteLine($"Количество цифр в строке {input.Length}"); 
+Console.WriteLine($"Количество цифр в строке {input.Length}");
+*/ 
+
+Console.WriteLine("Ведите Число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int result);
+if (isNumber)
+{
+    int count = 1;
+    while (result > 0)
+    {   
+        result /= 10;
+        count += 1;
+    }
+    Console.WriteLine($"Количество цифр в строке {count}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не число");
+}
