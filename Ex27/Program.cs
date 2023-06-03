1b636c4112ca579e67fd0302c2208fb49fa3d207
DmitriyIvaskov <citrys2008@gmail.com>
2023-06-03 19:36:29 +0700

    Cdelal 27

---
+++ Ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Ведите Число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int result);
+if (isNumber)
+{
+    int sum = 0;
+    while (result > 0)
+    {   
+        int length = result % 10;
+        result /= 10;
+        sum = sum + length ;
+    }
+    Console.WriteLine($"Сумма цифр {sum}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не число");
+}
