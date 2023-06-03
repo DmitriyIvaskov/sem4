2ce26da4b1bb8d05e6de3f01a22716521439f8c0
DmitriyIvaskov <citrys2008@gmail.com>
2023-06-03 19:18:01 +0700

    Cdelal 25

---
+++ Ex25/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите чсило A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int sum = A;
+
+for (int i = 1; i < B; i++)
+{
+    sum *= A;
+}
+Console.WriteLine($"Число A в степени числа B = {sum}");
